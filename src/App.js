af558f930cb47959127a845ba82458b83161a486
Wehan99 <wehanhimsara@gmail.com>
2023-09-21 23:02:59 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import testJs from './components/testJs';
+
+
+function App() {
+      return (
+        <div className="App">
+        testJs 
+     
+        </div>
+      );
+  } 
+
+  console.log("test results")
+
+export default App;
---
+++ src/components/testJs.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function testJs() {
+
+    return <h3><u>Hello World</u></h3>
+    
+}
+console.log("test results")
+console.log(testJs)
+export default testJs
